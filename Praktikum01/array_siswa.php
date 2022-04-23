b90bc823488da2ee6868e119767b0ea511ddf1fc
yudiansyah <77084763+yudiansyah25@users.noreply.github.com>
2022-04-23 13:49:29 +0700

    Add files via upload

---
+++ Praktikum01/array_siswa.php
@@ -0,0 +1,91 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>praktikum1</title>
+    <!-- css bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <style>
+    .nilai {
+        padding: 20px;
+    }
+    </style>
+</head>
+<body style="background-color: bisque;">
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">SITE</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item ">
+                        <a class="nav-link active" aria-current="page" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#feature">feature</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#social">social</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- jumbotron -->
+    <div class="jumbotron text-center">
+        <h1 class="display-4">Nilai Hasil Pembelajaran</h1>
+        <p class="lead">semester1</p>
+        <hr class="my-4">
+        <a class="btn btn-primary btn-lg" href="#nilai" role="button">Lihat Nilai</a>
+    </div>
+<section id="nilai">
+<div class="nilai">
+<!-- syntax php -->
+<?php
+$ns1 = ['id'=>1,'nim'=>'01101','uts'=>80,'uas'=>84,'tugas'=>78];
+$ns2 = ['id'=>2,'nim'=>'01121','uts'=>70,'uas'=>50,'tugas'=>68];
+$ns3 = ['id'=>3,'nim'=>'01130','uts'=>60,'uas'=>86,'tugas'=>70];
+$ns4 = ['id'=>4,'nim'=>'01134','uts'=>90,'uas'=>91,'tugas'=>82];
+
+$ar_nilai = [$ns1, $ns2 , $ns3, $ns4];
+
+?>
+<h3 style="text-align: center;">Daftar Nilai Siswa</h3>
+<h4 ><b>Note :</b> silahkan hubungi dosen untuk melakukan perbaikan.</h4>
+<table border="1" width="100%">
+    <thead>
+        <tr>
+            <th>No</th><th>NIM</th><th>UTS</th>
+            <th>UAS</th><th>Tugas</th><th>Nilai Akhir</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $nomor = 1;
+        foreach($ar_nilai as $ns) {
+            echo '<tr><td>'.$nomor.'</td>';
+            echo '<td>'.$ns['nim'].'</td>';
+            echo '<td>'.$ns['uts'].'</td>';
+            echo '<td>'.$ns['uas'].'</td>';
+            echo '<td>'.$ns['tugas'].'</td>';
+            $nilai_akhir = ($ns['uts'] + $ns['uas']+$ns['tugas'])/3;
+            echo '<td>'.number_format($nilai_akhir,2,',','.').'</td>';
+            echo '<tr/>';
+            $nomor++;
+        }
+        ?>
+</div>
+</tbody>
+</section>
+</body>
+</html>
